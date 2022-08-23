1e67d8d08ca8dfff4eb7b39b38525e32ff3d16a0
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2022-08-23 22:50:34 +0700

    update

--- assests/js/index.js
+++ assests/js/index.js
@@ -4,7 +4,7 @@ class button {
     #color
     #time
     #borderColor
-    
+
     constructor(btn, bgColor, color, time, borderColor) {
         this.#btn = btn;
         this.#bgColor = bgColor;
@@ -13,10 +13,22 @@ class button {
         this.#borderColor = borderColor;
     }
 
-    ClickEvent() {
-        const _this = this;
+    ChangeForm() {
+        $(".pomo-status-btn-active").addClass('pomo-status-btn')
+        $(".pomo-status-btn-active").removeClass('pomo-status-btn-active')
+        this.#btn.removeClass("pomo-status-btn")
+        this.#btn.addClass("pomo-status-btn-active")
 
-        _this.#btn.click(function () {
+        $('body').css("background-color", this.#bgColor)
+        $('.header-container').css("border-bottom", this.#borderColor)
+        $('.btn').css("color", this.#color)
+        $('#minute').html(`${this.#time.minute}`)
+        $('#second').html(`${this.#time.second}`)
+    }
+
+    ClickEvent() {
+        let _this = this
+        this.#btn.click(function () {
             $(".pomo-status-btn-active").addClass('pomo-status-btn')
             $(".pomo-status-btn-active").removeClass('pomo-status-btn-active')
             _this.#btn.removeClass("pomo-status-btn")
@@ -27,7 +39,11 @@ class button {
             $('.btn').css("color", _this.#color)
             $('#minute').html(`${_this.#time.minute}`)
             $('#second').html(`${_this.#time.second}`)
-        })  
+        })
+    }
+
+    getTime() {
+        return this.#time;
     }
 }
 class shortBreakButton extends button {
@@ -47,43 +63,51 @@ class pomoButton extends button {
 }
 
 const sbBtn = new shortBreakButton($(".btn-sb"), "var(--bg-color-sb)", "rgb(76, 145, 149)",
-    { minute: 5, second: "00" }, "1px solid #448286")
+    { minute: 1, second: "00" }, "1px solid #448286")
 
 const lbBtn = new longBreakButton($(".btn-lb"), "var(--bg-color-lb)", "rgb(69, 124, 163)",
     { minute: 15, second: "00" }, "1px solid #3e6f92")
 
 const pomoBtn = new pomoButton($(".btn-pomo"), "var(--bg-color-pomo)", "rgb(217, 85, 80)",
-    { minute: 25, second: "00" }, "1px solid #c34c48")
+    { minute: 1, second: "00" }, "1px solid #c34c48")
 
 
 const APP = {
     shortBreak: sbBtn,
     longBreak: lbBtn,
     pomo: pomoBtn,
-    refreshIntervalId: null, 
-    
+    refreshIntervalId: null,
+    breakTimes: 0,
+    currentStatus: null,
+
     handleEventOfButton: function () {
         this.shortBreak.ClickEvent();
         this.longBreak.ClickEvent();
         this.pomo.ClickEvent();
     },
 
-    handleTimer: function() {
-        _this = this
+    handleTimer: function () {
+        const _this = this
+        const time = this.pomo.getTime()
+
+        $('#minute').html(time.minute)
+        $('#second').html(time.second)
+
         $('.btn').click(function () {
             var audioElement = document.createElement('audio');
             audioElement.setAttribute('src', 'https://pomofocus.io/audios/button-press.wav');
             audioElement.play()
-        
+
             const text = $('.btn').text()
-        
+
             if (text == "START")
                 _this.startTimer()
             else _this.stopTimer()
         })
     },
 
-    startTimer: function() {
+    startTimer: function () {
+        const _this = this
         $('.btn').html("STOP")
         $('.btn').css("box-shadow", "none")
         $('.btn').css("top", "6px")
@@ -99,8 +123,33 @@ const APP = {
                 minute -= 1;
             }
 
-            if (minute == 0) {
+            if (second < 10) {
+                second = "0" + second;
+            }
 
+            if (minute == -1) {
+                _this.stopTimer()
+                _this.breakTimes += 1
+                _this.getCurrentStatus()
+
+                if (_this.currentStatus != pomo) {
+                    if (_this.breakTimes % 3 == 0) {
+                        _this.longBreak.ChangeForm()
+                        let time = _this.longBreak.getTime()
+                        minute = time.minute
+                        second = time.second
+                    } else {
+                        _this.shortBreak.ChangeForm()
+                        let time = _this.shortBreak.getTime()
+                        minute = time.minute
+                        second = time.second
+                    }
+                } else {
+                    _this.pomo.ChangeForm()
+                    let time = _this.pomo.getTime()
+                    minute = time.minute
+                    second = time.second
+                }
             }
 
             $('#minute').html(minute)
@@ -110,7 +159,7 @@ const APP = {
         this.refreshIntervalId = setInterval(timerFunction, 1000);
     },
 
-    stopTimer: function() {
+    stopTimer: function () {
         $('.btn').html("START")
         $('.btn').css("box-shadow", "rgb(235 235 235) 0px 6px 0px")
         $('.btn').css("top", "0")
@@ -120,8 +169,30 @@ const APP = {
     start: function () {
         this.handleEventOfButton();
         this.handleTimer();
+    },
+
+    getCurrentStatus: function () {
+        let temp = $('.pomo-status-btn-active').hasClass('.btn-pomo')
+        if (temp == true) {
+            this.currentStatus = "pomo";
+            return;
+        }
+
+        temp = $('.pomo-status-btn-active').hasClass('.btn-sb')
+        if (temp == true) {
+            this.currentStatus = "shortBreak";
+            return;
+        }
+
+        temp = $('.pomo-status-btn-active').hasClass('.btn-lb')
+        if (temp == true) {
+            this.currentStatus = "longBreak";
+            return;
+        }
     }
 }
 
-APP.start()
+$(document).ready(function () {
+    APP.start()
+})
 
